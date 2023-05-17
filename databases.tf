6ebe27d853877676ae8181c7b4bcbf77fddc15d8
Sunil Devarapu <sunil.devarapu1019@gmail.com>
2023-05-17 11:07:10 +0530

    terraform jenkins cicd base code

---
+++ databases.tf
@@ -0,0 +1,30 @@
+
+resource "aws_db_instance" "databasename" {
+    count =1
+    engine = "mysql"
+    instance_class = "db.t2.micro"
+    allocated_storage = 5
+    availability_zone = "us-east-1a"
+    db_name = "database${count.index+1}"
+    username = "admin"
+    password = "password"
+    apply_immediately = true
+    skip_final_snapshot = true
+    db_subnet_group_name = aws_db_subnet_group.subnet_group.name
+    vpc_security_group_ids = [aws_security_group.db_security_group.id]
+}
+
+resource "aws_db_instance" "databasename_2" {
+    count =1
+    engine = "mysql"
+    instance_class = "db.t2.micro"
+    allocated_storage = 5
+    availability_zone = "us-east-1b"
+    db_name = "database${count.index+1}"
+    username = "admin"
+    password = "password"
+    apply_immediately = true
+    skip_final_snapshot = true
+    db_subnet_group_name = aws_db_subnet_group.subnet_group.name
+    vpc_security_group_ids = [aws_security_group.db_security_group.id]
+}
---
+++ subnets.tf
@@ -0,0 +1,54 @@
+data "aws_availability_zones" "available" {}
+resource "random_integer" "random" {
+    min = 1
+    max = 100
+}
+
+resource "random_shuffle" "public_az" {
+    input = data.aws_availability_zones.available.names
+    result_count = 10
+}
+
+resource "aws_subnet" "web_server_subnet_1" {
+    vpc_id = aws_vpc.startup_vpc.id
+    availability_zone = "us-east-1"
+    cidr_block = "10.2.2.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "Web Server Subnet - 1"
+    }
+}
+
+resource "aws_subnet" "web_server_subnet_2" {
+    vpc_id = aws_vpc.startup_vpc.id
+    availability_zone = "us-east-1"
+    cidr_block = "10.2.4.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "Web Server Subnet - 2"
+    }
+}
+
+resource "aws_subnet" "database_subnet_1" {
+    vpc_id = aws_vpc.startup_vpc.id
+    availability_zone = "us-east-1"
+    cidr_block = "10.2.6.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "Database Subnet - 1"
+    }
+}
+
+resource "aws_subnet" "database_subnet_2" {
+    vpc_id = aws_vpc.startup_vpc.id
+    availability_zone = "us-east-1"
+    cidr_block = "10.2.8.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+        Name = "Database Subnet - 2"
+    }
+}
